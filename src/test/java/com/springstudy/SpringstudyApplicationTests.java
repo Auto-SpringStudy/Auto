06b557c3ecd70aa4a315c7bd8d5bf7938c36c214
Yunhyeok Choi <newh08@gmail.com>
2025-03-17 22:43:24 +0900

    테스트 삭제

--- src/test/java/com/springstudy/SpringstudyApplicationTests.java
+++ src/test/java/com/springstudy/SpringstudyApplicationTests.java
@@ -10,25 +10,5 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class SpringstudyApplicationTests {
-    @Value("${spring.datasource.url}")
-    String url;
 
-    @Value("${spring.datasource.username}")
-    String username = "sa";
-
-    @Value("${spring.datasource.password}")
-    String password = "sa";
-
-    @Test
-    void connectTest(){
-        try (Connection conn = DriverManager.getConnection(url, username, password)) {
-            if (conn != null) {
-                System.out.println("✅ 데이터베이스 연결 성공!");
-            } else {
-                System.out.println("❌ 데이터베이스 연결 실패!");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 }
