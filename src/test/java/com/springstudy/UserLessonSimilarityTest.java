fbb48833cacf8a8b2229ffdc69e58d2670afed0c
Yunhyeok Choi <newh08@gmail.com>
2025-04-04 21:40:09 +0900

    Version 0.1

--- src/test/java/com/springstudy/UserLessonSimilarityTest.java
+++ src/test/java/com/springstudy/UserLessonSimilarityTest.java
@@ -7,42 +7,32 @@ import com.springstudy.checker.SimilarityChecker;
 import com.springstudy.domain.UserLesson;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
 public class UserLessonSimilarityTest {
-
-    @Autowired
-    private SimilarityChecker similarityChecker;
 
     @Test
     void testUserLessonMarkedUsedWhenSimilar() {
-        // given
         String targetLesson = "1주차 과제";
+        SimilarityChecker similarityChecker = new SimilarityChecker();
 
-        UserLesson ul1 = new UserLesson(); // 유사하지 않음
-        ul1.setLesson("완전 다른 강의");
+        UserLesson ul1 = new UserLesson();
+        ul1.setLesson("수학 기초 강의"); // 확실히 유사하지 않은 단어
         ul1.setUsedForCheck(false);
 
-        UserLesson ul2 = new UserLesson(); // 유사한 제목
+        UserLesson ul2 = new UserLesson();
         ul2.setLesson("1 주차 과제");
         ul2.setUsedForCheck(false);
 
         List<UserLesson> lessons = List.of(ul1, ul2);
 
-        // when
         lessons.stream()
                 .filter(ul -> similarityChecker.isSimilar(ul.getLesson(), targetLesson))
                 .forEach(ul -> ul.setUsedForCheck(true));
 
+        System.out.println("ul1 similarity: " + similarityChecker.isSimilar(ul1.getLesson(), targetLesson));
+        System.out.println("ul2 similarity: " + similarityChecker.isSimilar(ul2.getLesson(), targetLesson));
 
-
-        // then
-        assertFalse(ul1.isUsedForCheck(), "완전 다른 강의는 false여야 한다");
-        assertTrue(ul2.isUsedForCheck(), "유사한 강의는 true로 바뀌어야 한다");
+        assertFalse(ul1.isUsedForCheck(), "유사하지 않은 강의는 false여야 함");
+        assertTrue(ul2.isUsedForCheck(), "유사한 강의는 true로 바뀌어야 함");
     }
 }
